7075541c81f7a6e875a31012b0f3399246aecab1
Michael Cullen <mc67everythingproperty@gmail.com>
2026-04-30 22:40:12 +0100

    Update refresh_data.py

--- refresh_data.py
+++ refresh_data.py
@@ -146,7 +146,7 @@ PP_MONTHLY_URL = "https://price-paid-data.publicdata.landregistry.gov.uk/pp-mont
 PP_ANNUAL_URL  = "https://price-paid-data.publicdata.landregistry.gov.uk/pp-{year}.csv"
 
 # All confirmed annual files
-PP_ANNUAL_YEARS = [2021, 2022, 2023, 2024]
+PP_ANNUAL_YEARS = [2021,]
 
 
 def get_latest_pp_url() -> str:
